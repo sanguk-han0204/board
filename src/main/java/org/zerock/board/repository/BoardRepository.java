2ff0a38879ca16bc609d7d95e64732e716f9edb2
sanguk_han <sanguk_han@tmax.co.kr>
2021-07-20 01:31:12 +0900

    init

---
+++ src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -0,0 +1,15 @@
+package org.zerock.board.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.zerock.board.entity.Board;
+
+import java.util.Objects;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+//한개의 row(object) 내에 object[] 로 나옴
+    @Query("select b, w from Board b left join b.writer w where b.bno =:bno")
+    Object getBoardWithWriter(@Param("bno") Long bno);
+
+}
