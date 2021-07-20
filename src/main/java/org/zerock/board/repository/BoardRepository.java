1a22c2a1ccfd4546ebba57b3107390007ab46cce
sanguk_han <sanguk_han@tmax.co.kr>
2021-07-20 21:53:37 +0900

    0720

--- src/main/java/org/zerock/board/repository/BoardRepository.java
+++ src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -1,10 +1,13 @@
 package org.zerock.board.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.zerock.board.entity.Board;
 
+import java.util.List;
 import java.util.Objects;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
@@ -14,5 +17,19 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Query("SELECT b, r from Board b left join Reply r on r.board = b where b.bno = :bno")
     List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    @Query(value="select b, w, count(r) "+
+    " from Board b " +
+    " left join b.writer w " +
+    " left join Reply r on r.board = b " +
+    " GROUP BY b",
+    countQuery = "select count(b) from Board b")
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable);
+
+    @Query("select b, w, count(r) "+
+    " from Board b left join b.writer w " +
+    " left outer join Reply r on r.board = b " +
+    " where b.bno = :bno")
+    Object getBoardByBno(@Param("bno") Long bno);
 
 }
