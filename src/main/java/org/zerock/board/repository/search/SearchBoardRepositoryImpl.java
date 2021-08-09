2a84d983c1acdcd4ec59000e092848d6cc0695db
sanguk_han <sanguk_han@tmax.co.kr>
2021-08-10 02:16:31 +0900

    210810

--- src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -1,8 +1,19 @@
 package org.zerock.board.repository.search;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.board.entity.Board;
+import org.zerock.board.entity.QBoard;
+import org.zerock.board.entity.QMember;
+import org.zerock.board.entity.QReply;
+
+import java.util.List;
 
 @Log4j2
 public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
@@ -13,8 +24,82 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
     @Override
     public Board search1(){
         log.info("search1.........");
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count());
+        tuple.groupBy(board);
+
+        log.info("-----------------------");
+        log.info(tuple);
+        log.info("-----------------------");
+
+        List<Tuple> result = tuple.fetch();
+
+
+        log.info(result);
 
         return null;
     }
 
+    @Override
+    public Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
+        log.info("searchPage....................................");
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        //select b, w, count(r) from board b
+        //left join b.writer w left join reply r on r.board = b
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member, reply.count());
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        BooleanExpression expression = board.bno.gt(0L);
+
+        booleanBuilder.and(expression);
+
+        if(type != null){
+            String[] typeArr = type.split("");
+            //검색조건 작성
+            BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+            for(String t:typeArr){
+                switch(t) {
+                    case "t":
+                        conditionBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "w":
+                        conditionBuilder.or(member.email.contains(keyword));
+                        break;
+                    case "c":
+                        conditionBuilder.or(board.content.contains(keyword));
+                        break;
+
+
+                }
+            }
+            booleanBuilder.and(conditionBuilder);
+        }
+
+        tuple.where(booleanBuilder);
+
+        tuple.groupBy(board);
+
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
+
+        return null;
+
+    }
 }
