a3ff23228bd0df6f30a6ad435fe72ec0c0385608
sanguk_han <sanguk_han@tmax.co.kr>
2021-08-13 18:22:46 +0900

    210813

--- src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -1,8 +1,14 @@
 package org.zerock.board.repository.search;
 
+import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.board.entity.Board;
+import org.zerock.board.entity.QBoard;
+import org.zerock.board.entity.QMember;
+import org.zerock.board.entity.QReply;
+
+import java.util.List;
 
 @Log4j2
 public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
@@ -14,7 +20,24 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
     public Board search1(){
         log.info("search1.........");
 
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        log.info("--------------------------");
+        log.info(jpqlQuery);
+        log.info("--------------------------");
+
+        List<Board> result = jpqlQuery.fetch();
+
         return null;
+
     }
 
 }
---
+++ src/main/java/org/zerock/board/service/ReplyService.java
@@ -0,0 +1,44 @@
+package org.zerock.board.service;
+
+import org.zerock.board.dto.ReplyDTO;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.Reply;
+
+import java.util.List;
+
+public interface ReplyService {
+    Long register(ReplyDTO replyDTO); //댓글 등록
+    List<ReplyDTO> getList(Long bno); //댓글 목록
+    void modify(ReplyDTO replyDTO); //댓글 수정
+    void remove(Long rno); //댓글 삭제
+
+    //ReplyDTO를 Reply객체로 변환 Board 객체의 처리가 수반됨
+    default Reply dtoToEntity(ReplyDTO replyDTO){
+        Board board = Board.builder().bno(replyDTO.getBno()).build();
+
+        Reply reply = Reply.builder().rno(replyDTO.getRno())
+                .text(replyDTO.getText())
+                .replyer(replyDTO.getReplyer())
+                .board(board)
+                .build();
+
+        return reply;
+    }
+
+    //Reply객체를 ReplyDTO로 변환, Board 객체가 필요없음
+
+    default ReplyDTO entityToDTO(Reply reply){
+        ReplyDTO dto = ReplyDTO.builder()
+                .rno(reply.getRno())
+                .text(reply.getText())
+                .replyer(reply.getReplyer())
+                .regDate(reply.getRegDate())
+                .modDate(reply.getModDate())
+                .build();
+
+        return dto;
+    }
+
+
+
+}
